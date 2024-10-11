64cfbecef269d1e1236d3f3e055ea9a94066ef6f
khanabdulla7862 <khanabdulla7862@gmail.com>
2024-10-11 21:52:32 +0530

    11 Oct 2024

---
+++ CurrentDate.java
@@ -0,0 +1,12 @@
+//Write a java program to find the current date in DD-MM-YYYY format.
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+public class CurrentDate {
+    public static void main(String[] args) {
+
+        LocalDate t = LocalDate.now();
+        DateTimeFormatter t1 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+       String fd=t.format(t1);
+        System.out.println(fd);
+    }
+}
