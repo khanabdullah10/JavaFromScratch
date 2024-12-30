f369fb137e1ecbbeeed107ebabf5666d95bfa65f
Khan Abdullah <khanabdullah@gmail.com>
2024-12-30 15:22:17 +0530

    Dec-30-31

---
+++ Java Practice/com/mcq/Ex1.java
@@ -0,0 +1,13 @@
+package com.mcq;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        int arr[] = new int[10];
+        for (int i = 0; i < 10; ++i) {
+            arr[i] = i;
+            System.out.print(arr[i]+ " ");
+            i++;
+        }
+    }
+}
